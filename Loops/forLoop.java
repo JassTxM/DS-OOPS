df89a454dcd482c48f1f335db88b6a389be24ed8
Jaskirat Singh <jasstxm@gmail.com>
2025-09-12 10:47:27 +0530

    [PATTERNS] | Pyramid and Pascal Patterns

--- Loops/forLoop.java
+++
@@ -1,8 +0,0 @@
-package Loops;
-public class forLoop {
-    public static void main(String[] args){
-        for(int i = 0;i<=10;i++){
-           System.out.println(i);
-        }
-    }
-}
---
+++ Patterns/pyramid.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.Scanner;
+public class pyramid {
+    public static void main(String[] args){
+        Scanner s1 = new Scanner(System.in);
+        int n = s1.nextInt();
+        for(int i = 1;i<=n;i++){
+            for (int j = 1; j<=n+1 - i ; j++ ){
+                System.out.print(" ");
+            }
+
+            for (int k = 1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        s1.close();
+    }
+}
