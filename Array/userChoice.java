8d90f7a80b96e39e594868a990800cc31ce7fab2
Jaskirat Singh <jasstxm@gmail.com>
2025-09-18 10:02:37 +0530

    [ARRAY] | Input, Update and Delete

--- Array/userChoice.java
+++ Array/userChoice.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class userChoice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+ 
         int num = input.nextInt();
         int[] arr = new int[num];
         for (int i = 0; i < num; i++) {
